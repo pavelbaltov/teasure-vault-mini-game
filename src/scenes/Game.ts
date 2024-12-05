2b8eda5ac05487a15f9febde9799f59a926ce51d
Pavel <ge42zeg@mytum.de>
2024-12-05 23:42:04 +0200

    feat: integration of code generationg and progress into game scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,8 +1,13 @@
+import Logger from "../core/Logger";
 import Scene from "../core/Scene";
+import { VaultSafeCodeGeneration } from "../prefabs/VaultSafeCodeGeneration";
+import { VaultSafeCodeProgress } from "../prefabs/VaultSafeCodeProgress";
 
 export default class Game extends Scene {
   name = "Game";
 
+  private vaultSafeCodeGeneration!: VaultSafeCodeGeneration;
+  private vaultSafeCodeProgress!: VaultSafeCodeProgress;
 
   load() {
 
@@ -10,7 +15,27 @@ export default class Game extends Scene {
   }
 
   async start() {
+    this.vaultSafeCodeGeneration = new VaultSafeCodeGeneration();
+    this.vaultSafeCodeProgress = new VaultSafeCodeProgress(this.vaultSafeCodeGeneration.secretCombinations);
+    this.vaultSafeCodeProgress.removeAllListeners();
+
+    this.vaultSafeCodeProgress.on("fail", this.onCodeFailed);
+    this.vaultSafeCodeProgress.on("complete", this.onCodeCompleted);
+
+    Logger.getInstance().log(this.vaultSafeCodeGeneration.toString());
   }
+
+  private async onCodeFailed() {
+
+    
+  }
+
+
+  private async onCodeCompleted() { 
+
+    
+  }
+
 
   onResize(width: number, height: number) {}
 }
